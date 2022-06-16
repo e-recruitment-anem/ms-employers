becd941b7a35155f5d1e737665835451bef4586d
aymenzitouni <a.zitouni@esi-sba.dz>
2022-06-16 14:20:36 +0100

    init project

---
+++ src/main/java/erecruitmentanem/msemployers/MsEmployersApplication.java
@@ -0,0 +1,13 @@
+package erecruitmentanem.msemployers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmployersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmployersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/erecruitmentanem/msemployers/MsEmployersApplicationTests.java
@@ -0,0 +1,13 @@
+package erecruitmentanem.msemployers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmployersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
